155513c87f37caad8d7986b3de4c15eabeb207a0
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-05-13 17:51:27 +0900

    practice_food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types"
+const foodILike=[
+  {
+    id:1,
+    name:'pepe',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vGxoyTuvBPe_iUK1M3l8xwHaEK%26pid%3DApi&f=1',
+    rating:5.0
+  },
+  {
+    id:2,
+    name:'steak',
+    image:'https://natashaskitchen.com/wp-content/uploads/2020/03/Pan-Seared-Steak-4.jpg',
+    rating:4.8
+  },
+  {
+    id:3,
+    name:'coffee',
+    image:'https://i.pinimg.com/originals/4c/d0/0e/4cd00ea918c1791ab9e04f1ef660806f.jpg',
+    rating:4.5
+  }
+]
+function Food({name, pic,rating}){
+  return (
+  <div>
+    <h2>I love {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={pic} alt={name}/>
+  </div>
+  );
+}
+Food.propTypes={
+  name:PropTypes.string.isRequired,
+  pic:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} pic={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
