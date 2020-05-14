3f1351831535c4152a9f87bccc033c72a07ed044
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-05-15 02:10:44 +0900

    html 정리

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types"
 
-function Movie({id,year,title,summary,poster}){
-    return <h4>{title}</h4>;
+function Movie({id,year,title,summary,poster,genres}){
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                {genres.map ( (genre,index) => (
+                    <li key={index} className="movie__genres">
+                        {genre}</li>
+                ))}
+                </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.protoTypes={
@@ -10,7 +25,8 @@ Movie.protoTypes={
     year:PropTypes.number.isRequired,
     title:PropTypes.string.isRequired,
     summary:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
