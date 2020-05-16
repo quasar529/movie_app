ee6d0dbb4bff475c74ccb41bdbe95d0b0caa0420
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-05-16 23:45:48 +0900

    Updates

---
+++ precache-manifest.81f7a6f4ff519ab2d40a51b4bdb49449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad71efa4880b05c8f9d59fa0ef8fb24",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "942901d58350f40ae8e9",
+    "url": "/movie_app/static/css/main.1d5531c2.chunk.css"
+  },
+  {
+    "revision": "f66daf3eed1d27073954",
+    "url": "/movie_app/static/js/2.6faa7bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.6faa7bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942901d58350f40ae8e9",
+    "url": "/movie_app/static/js/main.67f82c89.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
